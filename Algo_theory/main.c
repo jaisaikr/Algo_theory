5d9c31cf5f840256d96fed435625a5caa11330a8
JAISAIKRISHNAN <60820625+jaisai6@users.noreply.github.com>
2021-03-30 23:25:00 +0530

    Update main.c

--- Algo_theory/main.c
+++ Algo_theory/main.c
@@ -7,29 +7,40 @@
 
 // Global variables
 
+// size_A -- no. of nodes in set A
+// size_B -- no. of nodes in set B
+// num_edges -- total no. of edges from set A to set B
 uint size_A, size_B, num_edges;
 
+// This is used to store the degre of every node in the Graph
 uint adj_size[MAX_INSTANCE_SIZE];
 
+// This stores the adjacency info about every node in the Graph
 uint graph[MAX_INSTANCE_SIZE][MAX_INSTANCE_SIZE];
 
+// This stores the info of the matching
+// It stores UNMATCHED if the node is not matched to any other vertex
 uint matched[MAX_INSTANCE_SIZE];
 
+// This stores the info whether a node has been visited or not
 bool visited[MAX_INSTANCE_SIZE];
 
+// This struct stores the type of the set and the id of the node.
 typedef struct _vertex {
   char type;
   uint id;
 } vertex;
 
-// converts the vertex to uid...
 
+// The following two functions helps us to store the nodes of both sets
+// in a single array
+
+// converts the vertex to uid...
 uint vertex_to_uid(vertex v) {
   return ((v.type == 'A') ? 0 : size_A) + v.id;
 }
 
 // converts the uid to vertex...
-
 vertex uid_to_vertex(uint uid) {
   vertex ret;
   if (uid < size_A) {
@@ -42,8 +53,10 @@ vertex uid_to_vertex(uint uid) {
   return ret;
 }
 
-// Insert Function...
 
+
+// Insert Function...
+// This function adds edge between two nodes from set A and set B
 void insert(uint index, uint val){
 
     graph[index][adj_size[index]] = val;
@@ -52,7 +65,6 @@ void insert(uint index, uint val){
 }
 
 // augmenting_path function...
-
 bool augment_path(uint uid) {
   visited[uid] = true;
 
@@ -70,7 +82,8 @@ bool augment_path(uint uid) {
     }
 
     else if(matched[nbr] != uid){
-
+      // This is a simple DFS algorithm but we only apply DFS if there exists an augmenting path from the martched vertex of the neighbouring node.
+      
       visited[nbr] = true;
 
       if(augment_path(matched[nbr])){
@@ -85,21 +98,22 @@ bool augment_path(uint uid) {
 }
 
 int main(){
+  
     
+    // Initializing the matched array with every vertex as UNMATCHED...
     for(int i=0;i<MAX_INSTANCE_SIZE;i++){
         matched[i] = UNMATCHED;
     }
     
-    // Initializing size matrix...
+    // Initializing the adjacency size matrix with zero...
+    // This stores the degree of each node
     for(int i=0;i<MAX_INSTANCE_SIZE;i++){
         adj_size[i] = 0;
     }
-
-  // Standard competition practice of using cstdio instead of iostream
+  
+  // Reading input...
   scanf("%d %d %d\n", &size_A, &size_B, &num_edges);
 
-  // Reading input
-  
   vertex vertex_A, vertex_B;
   uint uid_A, uid_B;
   uint i;
@@ -117,9 +131,12 @@ int main(){
 
     insert(uid_B, uid_A);
   }
-
+  
+  // Initializing the size of matching...
   int size_matching = 0;
 
+  
+  // Checking for augmenting path from each node wrt current matching
   for(int i=0;i<size_A;i++){
     if(matched[i] == UNMATCHED){
       
@@ -132,7 +149,8 @@ int main(){
       }
     }
   }
-
+  
+  // Output
   printf("The size of the matching is: %d \n", size_matching);
 
   for(int i=0;i<size_A;i++){
